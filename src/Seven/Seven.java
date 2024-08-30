1a10e3b207e23035ddf0602aa61107c562365c5a
Nuongdang <dangthinuong1804@gmail.com>
2024-08-30 13:50:51 +0700

    chang seven.java

--- src/Seven/Seven.java
+++ src/Seven/Seven.java
@@ -3,5 +3,8 @@ package Seven;
 public class Seven {
     public static void main(String[] args){
         System.out.println("Seven");
+        System.out.println("Seven1");
+        System.out.println("Seven2");
+
     }
 }
